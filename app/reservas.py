10d21c18427b76d21e063ea50f09c549be239fd9
RenzoMedina <medinast30@gmail.com>
2025-06-25 22:13:48 -0400

    Remove files now ignored by .gitignore

--- app/reservas.py
+++
@@ -1,5 +0,0 @@
-def verificarDisponibilidad(reservas, nueva):
-    for r in reservas:
-        if r['sala'] == nueva['sala'] and r['hora'] == nueva['hora']:
-            return False
-    return True
