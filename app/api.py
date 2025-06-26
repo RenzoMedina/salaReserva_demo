33b598c7cf2db08b20b3136c7440ef88ead21218
RenzoMedina <medinast30@gmail.com>
2025-06-25 22:15:59 -0400

    Add files

---
+++ app/api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from app.reservas import verificarDisponibilidad
+
+app = Flask(__name__)
+reservas = []
+
+@app.route("/reservar", methods=['POST'])
+def reservar():
+    data = request.get_json()
+    disponible = verificarDisponibilidad(reservas,data)
+
+    if disponible:
+        reservas.append(data)
+        return jsonify({"mensaje:","Reservada con éxito"}), 201
+    else:
+        return jsonify({"mensaje:","Sala no disponible"}), 409
